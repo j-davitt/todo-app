54a5ec0aa0bad23d111b54cd314fa94c401be0c1
Joe Davitt <joseph.j.davitt@gmail.com>
2023-03-29 18:08:34 +0000

    change styles

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { createStyles, Header, Navbar } from "@mantine/core";
+import { createStyles, Header, Navbar, Group } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 
@@ -6,21 +6,27 @@ const useStyles = createStyles((theme) => ({
   nav: {
     backgroundColor: theme.colors.blue[6],
     height: '100%',
-    margin: 'auto',
-    color: 'white',
+    
+  },
+  link: {
+    color: theme.colors.gray[0],
+    textDecoration: 'none',
+    padding: theme.spacing.md,
   }
 }))
 
 const HeaderComp = (props) => {
   const { classes } = useStyles();
-  
+
 
   return (
 
     <Header >
       <Navbar className={classes.nav}>
-        <Link to='/'>Home</Link>
-        <Link to='/settings'>Settings</Link>
+        <Group>
+          <Link className={classes.link} to='/'>Home</Link>
+          <Link className={classes.link} to='/settings'>Settings</Link>
+        </Group>
       </Navbar>
     </Header>
 
--- src/Components/Settings/index.jsx
+++ src/Components/Settings/index.jsx
@@ -1,38 +1,76 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SettingsContext } from "../../Context/Settings";
+import { createStyles, Grid } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  h1: {
+    backgroundColor: theme.colors.gray[8],
+    color: theme.colors.gray[0],
+  },
+  div: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    padding: theme.spacing.md,
+  },
+  section: {
+    border: `3px solid ${theme.colors.gray[8]}`,
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing.md,
+  }
+}));
+
 
 
 const Settings = (props) => {
+  const { classes } = useStyles();
 
   const { pageItems, showCompleted, sort, setSort, setPageItems, setShowCompleted, saveLocalStorage } = useContext(SettingsContext);
 
+  const [showUpdate, setShowUpdate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setShowUpdate(true);
     saveLocalStorage();
   };
-  
+
 
   return (
     <>
-      <h1>Manage Settings</h1>
-      <form onSubmit={handleSubmit} >
-        <label>
-          <span>Show Completed?</span>
-          <input type="checkbox" name="showCompleted" checked={showCompleted} onChange={(e) => setShowCompleted(e.target.checked)} />
-        </label>
-        <label>
-          <span>Items per page</span>
-          <input type="number" name="pageItems" value={pageItems} onChange={(e) => setPageItems(e.target.value)} />
-        </label>
-        <label>
-          <span>Sort by</span>
-          <input type="text" name="sort" value={sort} onChange={(e) => setSort(e.target.value)} />
-        </label>
-        <label>
-          <button type="submit">Submit</button>
-        </label>
-      </form>
+
+      <h1 className={classes.h1}>Manage Settings</h1>
+      <div className={classes.div}>
+        <section className={classes.section}>
+        <h3>Update Settings</h3>
+          <form onSubmit={handleSubmit} className={classes.form} >
+            <label>
+              <span>Show Completed?</span>
+              <input type="checkbox" name="showCompleted" checked={showCompleted} onChange={(e) => setShowCompleted(e.target.checked)} />
+            </label>
+            <label>
+              <span>Items per page</span>
+              <input type="number" name="pageItems" value={pageItems} onChange={(e) => setPageItems(e.target.value)} />
+            </label>
+            <label>
+              <span>Sort by</span>
+              <input type="text" name="sort" value={sort} onChange={(e) => setSort(e.target.value)} />
+            </label>
+            <label>
+              <button type="submit">Update Settings</button>
+            </label>
+          </form>
+        </section>
+        {showUpdate &&
+          <section className={classes.section}>
+            <h3>Updated Settings</h3>
+            <p>Show Completed: {showCompleted ? 'yes' : 'no'}</p>
+            <p>Items per page: {pageItems}</p>
+            <p>Sort by: {sort}</p>
+          </section>}
+      </div>
     </>
   )
 
